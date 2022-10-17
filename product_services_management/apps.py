c7cd207d6a3aa904f03807c835984dfed422d568
maik-Jar <maikrivera01@gmail.com>
2022-10-17 17:29:42 -0400

    inicio de proyecto y creacion de apps

---
+++ product_services_management/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ProductServicesManagementConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'product_services_management'

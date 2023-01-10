8b4633fd4ee19f62b00c76821f1c410ef8af7916
Brian Rivera <maikrivera01@gmail.com>
2023-01-10 19:47:41 -0400

    correction migration 0005

--- customers_management/migrations/0005_tipocliente_alter_cliente_tipo.py
+++ customers_management/migrations/0005_tipocliente_alter_cliente_tipo.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='TipoCliente',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('nombre', models.CharField(max_length=20)),
                 ('estado', models.BooleanField(default=True)),
                 ('creado', models.DateTimeField(auto_now_add=True)),
